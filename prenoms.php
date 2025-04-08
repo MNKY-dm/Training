7d4f317c718b1a7641823903560e45ee1956d7d7
Dylan <dylan.moulin9272@gmail.com>
2025-04-08 15:47:27 +0200

    Arrangement du code en rangeant par fonction... (prenoms.php)

--- prenoms.php
+++ prenoms.php
@@ -14,7 +14,7 @@
         <br>
         <label for="dateNaiss">
             Saisissez votre date de naisance : 
-            <input type="date" name="dateNaiss" id="dateNaiss">
+            <input type="number" name="dateNaiss" id="dateNaiss">
         </label>
         <br>
         <input type="reset" value="Réinitialiser">
@@ -25,8 +25,90 @@
 </html>
 
 <?php
-if ((isset($_GET['prenom'])) && ($_GET['dateNaiss'])){
-    $prenom = $_GET['prenom'];
-    $dateNaiss = $_GET['dateNaiss'];
-    echo "Ton nom est ".$prenom.'.'."<br>Tu es né le ".$dateNaiss;
+function br() {
+    echo "<br>";
 }
+
+findByYear();
+
+// if ((isset($_GET['prenom'])) && (isset($_GET['dateNaiss']))){
+//     $file = fopen("../prenoms/DS_PRENOMS_data_2.csv","r");
+
+//     $prenom = htmlspecialchars($_GET['prenom']);
+//     $dateNaiss = htmlspecialchars($_GET['dateNaiss']);
+
+//     echo "Ton nom est ".$prenom.'.'."<br>Tu es né en ".$dateNaiss;
+//     br();
+    
+//     $correspond = 0;
+//     $correspondAnnee = 0;
+
+//     while (($data = fgetcsv($file, NULL, ";")) !== FALSE) {
+
+//         // var_dump($data); --> "YOB;"SEX";"PRENOM";"GEO";"GEO_OBJECT";"TIME_PERIOD";"OBS_VALUE"
+
+//         $dep = $data[3];
+//         $nbParDep = $data[6];
+
+//         if (strtoupper($prenom) === $data[2]){
+
+//             $correspond+= (int)str_replace(",","",$data[6]);
+
+//             if ($dateNaiss === $data[0]) {
+//                 $correspondAnnee+= (int)str_replace(",","",$data[6]);
+//             }
+            
+//             if ($dep === 'DEP') {
+//                 echo "Votre prénom a été donné ".$nbParDep." fois dans le département n°".$dep;
+//             }
+//         }
+//     }
+//     br();
+//     echo "Votre prénom a été donné ".$correspondAnnee." fois en ".$dateNaiss;
+//     br();
+//     echo "Votre prénom a été donné ".$correspond." fois au total depuis 20 ans en France.";
+    
+
+//     fclose($file);
+// }
+
+function findByYear() {
+    if ((isset($_GET['prenom'])) && (isset($_GET['dateNaiss']))){
+        $file = fopen("../prenoms/DS_PRENOMS_data.csv","r");
+    
+        $prenom = htmlspecialchars($_GET['prenom']);
+        $dateNaiss = htmlspecialchars($_GET['dateNaiss']);
+    
+        echo "Ton nom est ".$prenom.'.'."<br>Tu es né en ".$dateNaiss;
+        br();
+        
+        $correspondAnnee = 0;
+    
+        while (($data = fgetcsv($file, NULL, ";")) !== FALSE) {
+    
+            // var_dump($data); --> "YOB;"SEX";"PRENOM";"GEO";"GEO_OBJECT";"TIME_PERIOD";"OBS_VALUE"    
+            if (strtoupper($prenom) === $data[2]){    
+                if ($dateNaiss === $data[0]) {
+                    $correspondAnnee+= (int)str_replace(",","",$data[6]);
+                    if ($data[4] === 'DEP'){
+                        echo $data[3]." : ".(int)str_replace(",","",$data[6]);
+                        br();
+                    }
+                }
+            }
+        }
+        br();
+        echo "Votre prénom a été donné ".$correspondAnnee." fois en ".$dateNaiss;
+        br();
+        
+        
+        fclose($file);
+    }
+}
+
+
+
+// if ($dep === 'DEP') {
+//     echo "Votre prénom a été donné ".$nbParDep." fois dans le département n°".$dep;
+//     echo "Votre prénom a été donné ".$correspond." fois au total depuis 20 ans en France.";
+// }
